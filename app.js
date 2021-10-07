43d981b85e461b2135ed773ce90aac1423521472
tastytim <tastytimgm@gmail.com>
2021-10-07 13:47:11 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+function alertDates() {
+  let nome = prompt("Nome?");
+  let cognome = prompt("Cognome?");
+  let colore = prompt("Colore preferito?");
+
+  if (
+    nome === null ||
+    cognome === null ||
+    colore === null ||
+    nome.length == 0 ||
+    cognome.length == 0 ||
+    colore.length == 0
+  ) {
+    alert("Hai inserito i dati sbagliati, riprova");
+    alertDates();
+  } else 
+  {
+    let result = `Nome: ${nome}, Cognome: ${cognome}, Colore preferito: ${colore}`;
+    alert(result);
+  }
+}
+
+alertDates();
